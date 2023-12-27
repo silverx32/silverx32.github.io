1e605e1f333157420de8298b55d1a2ffdcf29a80
buyan <2498121302@qq.com>
2023-12-27 16:30:43 +0800

    2

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -3,7 +3,7 @@ import type { DefaultThemeOptions } from "vuepress";
 import recoTheme from "vuepress-theme-reco";
 
 export default defineUserConfig({
-  title: "vuepress-theme-reco",
+  title: "我麻了啊",
   description: "Just playing around",
   theme: recoTheme({
     style: "@vuepress-reco/style-default",
