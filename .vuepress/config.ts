82e8ee4e0c8cb2a9f29f6f5930b693fa79199044
buyan <2498121302@qq.com>
2024-01-15 15:51:08 +0800

    update

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -38,7 +38,7 @@ export default defineUserConfig({
     },
     // 当 autoAddCategoryToNavbar 为 true 时，则全部取默认值
     navbar:[
-      {text:"猫国建设者",link:"/Game/kittenGame"},
+      {text:"猫国建设者",link:"/Game/kittenGame/"},
     ]
     // navbar: [
     //   { text: "博客", link: "/posts" },
